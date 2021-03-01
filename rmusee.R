147de05cb39c691d26d29f3dc22f063ca4ee5e22
M.Hamelin <79644158+MaHamelin@users.noreply.github.com>
2021-03-01 18:35:29 +0100

    Update rmusee.R

--- rmusee.R
+++ rmusee.R
@@ -33,3 +33,4 @@ loc_musee <- read_delim('liste-et-localisation-des-musees-de-france.csv',delim =
 loc_musee <- loc_musee %>% select(c(2,6,8,14,15))
 musee <- inner_join(loc_musee, museefreq, by = c('ref_musee'='id'))µ
 #TEST BG
+#non pas bg
