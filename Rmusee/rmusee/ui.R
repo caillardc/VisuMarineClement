1204db62d3f7a55f7a9e848c4100c692894477b7
caillardc <50209833+caillardc@users.noreply.github.com>
2021-03-08 14:32:29 +0100

    Update ui.R

--- Rmusee/rmusee/ui.R
+++ Rmusee/rmusee/ui.R
@@ -46,57 +46,57 @@ shinyUI(bootstrapPage(
                    
                    
                ))),
-             tabPanel(title = 'Cartographie',
-                      div(class = 'outer',  
-                        tags$style(type = "text/css", "div.outer {position: fixed; top: 55px; left: 0; right: 0; bottom: 0;}"),
-                        tags$style(type = "text/css", "#controls {background-color: rgba(255,255,255,0.8); padding: 20px; border-radius: 8px}"),
-                        tags$style(type = "text/css", "label {font-weight: 400;}"),
-                        
-                        leafletOutput(outputId = "map", width="100%", height="100%"),
-                        absolutePanel(id = "controls", bottom = 100, right = 55, width = 250, fixed=TRUE, height = "auto",
-                                      sliderInput("visite", "Visiteurs en 2018 de 0 à plus de 1M", min(musee$total.2018), 1000000,
-                                                  value = c(min(musee$total.2018), 1000000), step = 100),
-                                      textInput('zoom', "Zoom sur une adresse", value = ""),
-                                      actionButton("button", "Recherche"),
-                                      p(""),
-                                      selectInput(inputId = "regioncarte", 
-                                                  label = "Sélectionner une région :", 
-                                                  choices = c('', musee %>% group_by(region) %>% summarise(region = unique(region))))
-                                      
-                        )
-                      )
-             ),
-             tabPanel(title = 'Statistique descriptive',
-                      fluidRow(column(width = 4,
-                                      wellPanel(style = "background-color: #fff; border-color: #2c3e50",
-                                                sliderInput("annee",
-                                                            "Année :",
-                                                            min = 2013,
-                                                            max = 2018,
-                                                            value = 2018,
-                                                            step = 1,
-                                                            sep = ''),
-                                                selectInput(inputId = "region", 
-                                                            label = "Sélectionner une région :", 
-                                                            choices = musee %>% group_by(region) %>% summarise(region = unique(region)))
-                                      ),
-                                      fluidRow(column(12,
-                                                      wellPanel(style = "background-color: #fff; border-color: #2c3e50; height: 525px",
-                                                                chooseSliderSkin('Flat', color = '#26C4EC'),
-                                                                selectInput(inputId = "dpt", 
-                                                                            label = "Sélectionner un département :", 
-                                                                            choices = musee %>% group_by(departement) %>% summarise(departement = unique(departement)),
-                                                                            selected = 'PARIS'),
-                                                                DTOutput(outputId = "table")
-                                                      )))
-                      ),
-                               column(8,
-                                      amChartsOutput(outputId = 'histo'),
-                                      amChartsOutput(outputId = 'graph'))),
-                      )
-                      
-                      
-                      
-                      )
+    tabPanel(title = 'Cartographie',
+             div(class = 'outer',  
+                 tags$style(type = "text/css", "div.outer {position: fixed; top: 55px; left: 0; right: 0; bottom: 0;}"),
+                 tags$style(type = "text/css", "#controls {background-color: rgba(255,255,255,0.8); padding: 20px; border-radius: 8px}"),
+                 tags$style(type = "text/css", "label {font-weight: 400;}"),
+                 
+                 leafletOutput(outputId = "map", width="100%", height="100%"),
+                 absolutePanel(id = "controls", bottom = 100, right = 55, width = 250, fixed=TRUE, height = "auto",
+                               sliderInput("visite", "Visiteurs en 2018 de 0 à plus de 1M", min(musee$total.2018), 1000000,
+                                           value = c(min(musee$total.2018), 1000000), step = 100),
+                               textInput('zoom', "Zoom sur une adresse", value = ""),
+                               actionButton("button", "Recherche"),
+                               p(""),
+                               selectInput(inputId = "regioncarte", 
+                                           label = "Sélectionner une région :", 
+                                           choices = c('', musee %>% group_by(region) %>% summarise(region = unique(region))))
+                               
+                 )
              )
+    ),
+    tabPanel(title = 'Statistique descriptive',
+             fluidRow(column(width = 4,
+                             wellPanel(style = "background-color: #fff; border-color: #2c3e50",
+                                       sliderInput("annee",
+                                                   "Année :",
+                                                   min = 2013,
+                                                   max = 2018,
+                                                   value = 2018,
+                                                   step = 1,
+                                                   sep = ''),
+                                       selectInput(inputId = "region", 
+                                                   label = "Sélectionner une région :", 
+                                                   choices = musee %>% group_by(region) %>% summarise(region = unique(region)))
+                             ),
+                             fluidRow(column(12,
+                                             wellPanel(style = "background-color: #fff; border-color: #2c3e50; height: 525px",
+                                                       chooseSliderSkin('Flat', color = '#26C4EC'),
+                                                       selectInput(inputId = "dpt", 
+                                                                   label = "Sélectionner un département :", 
+                                                                   choices = musee %>% group_by(departement) %>% summarise(departement = unique(departement)),
+                                                                   selected = 'PARIS'),
+                                                       DTOutput(outputId = "table")
+                                             )))
+             ),
+             column(8,
+                    amChartsOutput(outputId = 'histo'),
+                    amChartsOutput(outputId = 'graph'))),
     )
+    
+    
+    
+  )
+)
+)
